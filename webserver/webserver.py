fba3c2216b5fc2725644f8339824b5f3b4637482
Hunniche <lklev16@student.sdu.dk>
2020-03-09 22:10:35 +0100

    asd

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, jsonify, make_response
 from child import Child
-import csv
-import json
+import csv, json, time
 
 app = Flask(__name__)
 children = []
+has_update = True
 
-def get_default_response(message):
+def get_default_response(message = ''):
     resp = make_response(message)
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
+
 
 def init_child_list():
     global children
@@ -27,27 +28,40 @@ def index():
 
 @app.route("/check", methods=["POST", "GET"])
 def check_child_in():
+    global has_update
     child_id = request.args.get("cid")
     for child in children:
         if child.id == child_id:
             if child.status:
                 child.status = not child.status
+                has_update = True
                 return get_default_response("Checking " + child.name + " in.")
             else:
                 child.status = not child.status
+                has_update = True
                 return get_default_response("Checking " + child.name + " out.")
     return get_default_response("No child found with that ID...")
 
 
 @app.route("/status", methods=["GET"])
 def daycare_status():
-    global children
+    global children, has_update
+    wait_counter = 0
+    while not has_update and wait_counter < 60: #one long-polling "session" lasts 1 minute
+        time.sleep(1)
+        wait_counter += 1
+        print("Long polling, and waiting for updates...")
+    
+    if not has_update:
+        get_default_response()
+
     children_j = [child.__dict__ for child in children]
     children_json = {
         "children": children_j
     }
+    has_update = False
     return get_default_response(jsonify(children_json))
 
 if __name__ == "__main__":
     init_child_list()
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080, threaded=True)
