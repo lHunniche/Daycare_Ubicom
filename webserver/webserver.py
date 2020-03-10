9ad912e4d45e9d8e867262f5507f1c5f8afeb459
Lasse Hunniche Klevang <lasse96-06@hotmail.com>
2020-03-10 11:22:35 +0100

    Update webserver.py

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -47,6 +47,7 @@ def check_child_in():
 def daycare_status():
     global children, has_update
     wait_counter = 0
+    print("Do we have updates? ", has_update)
     while not has_update and wait_counter < 60: #one long-polling "session" lasts 1 minute
         time.sleep(1)
         wait_counter += 1
